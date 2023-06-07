8ff7e4ca073be852bf4a8762f799f9a471d3fb81
mre-dev <mre.dev01@gmail.com>
2023-06-07 18:09:14 +0330

    chore: Complete Loading component

--- src/components/Loading/Loading.component.tsx
+++ src/components/Loading/Loading.component.tsx
@@ -1,5 +1,9 @@
+import { Spinner, Stack } from "@chakra-ui/react"
+
 export const Loading = () => {
   return (
-    <span>Loading ...</span>
+    <Stack direction='row' spacing={4} h="100vh" w="100vw" top={0} left={0} pos="absolute" justifyContent="center" alignItems="center">
+      <Spinner size='xl' />
+    </Stack>
   )
 }
