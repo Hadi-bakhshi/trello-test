a5c2090921ceb9071f3f9a926c2438e3cc22ca8e
mre-dev <mre.dev01@gmail.com>
2023-06-07 22:16:12 +0330

    feat: Improve 404 page with Chakra UI Components and useNavigate hook
    Added Text And Button to enhance the appearance of the 404 page and used useNavigate hook to directly redirect users to the home page. Updated the page content and added a button for better user experience.

--- src/pages/NotFound/Not-Found.page.tsx
+++ src/pages/NotFound/Not-Found.page.tsx
@@ -1,9 +1,19 @@
+import { Box, Button, Text } from "@chakra-ui/react";
+import { PATHS } from "configs";
 import { PageContainer } from "layouts";
+import { useNavigate } from "react-router-dom";
 
 const NotFoundPage = () => {
+  const Navigate = useNavigate();
+
   return(
     <PageContainer>
-      Not Found Page
+      <Box>
+        <Text fontSize="35px" fontWeight="bold" mb="10px">404 Not Found</Text>
+        <Button onClick={() => {
+          Navigate(PATHS.HOME);
+        }}>Goto Home</Button>
+      </Box>
     </PageContainer>
   )
 }
