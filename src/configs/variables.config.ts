21cf9ccfae2255ceb71bc1fbde8d41ef4367e14f
mre-dev <mre.dev01@gmail.com>
2023-06-07 16:38:00 +0330

    fix: Use import.meta.env instead of process.env for accessing environment variables

--- src/configs/variables.config.ts
+++ src/configs/variables.config.ts
@@ -1,2 +1,2 @@
-export const APP_ENVIORMENT = process.env.REACT_APP_APP_ENVIORMENT || 'development';
-export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'https://618ace5834b4f400177c48c0.mockapi.io';
+export const APP_ENVIORMENT = import.meta.env.REACT_APP_APP_ENVIORMENT || 'development';
+export const API_BASE_URL = import.meta.env.REACT_APP_API_BASE_URL || 'https://618ace5834b4f400177c48c0.mockapi.io';
