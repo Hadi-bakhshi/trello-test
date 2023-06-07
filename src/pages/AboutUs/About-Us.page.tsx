8cd905b3d91d21ea5ba2b875728b2f46a5304f35
mre-dev <mre.dev01@gmail.com>
2023-06-07 22:23:19 +0330

    feat: Fix About Us page

--- src/pages/AboutUs/About-Us.page.tsx
+++ src/pages/AboutUs/About-Us.page.tsx
@@ -1,9 +1,13 @@
+import { Box, Text } from "@chakra-ui/react";
 import { PageContainer } from "layouts";
 
 const AboutUsPage = () => {
   return(
     <PageContainer>
-      About Us Page
+      <Box>
+        <Text fontSize="25px" fontWeight="bold" borderBottom="1px solid white" w="fit-content" m="0 auto">Behineh Kavan Keyfiat</Text>
+        <Text>Mohammad Reza Ebrahimi</Text>
+      </Box>
     </PageContainer>
   )
 }
